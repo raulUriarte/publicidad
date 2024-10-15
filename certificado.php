b5babe1948c9edc37eedd697a8b2df67e40d90ab
raulUriarte <raul.uc7@gmail.com>
2024-10-15 11:21:28 -0500

    w

--- certificado.php
+++ certificado.php
@@ -2,5 +2,5 @@
 $cedula = $_GET['cedula'];
 $nombres = $_GET['nombres'];
 $apellidos = $_GET['apellidos'];
-echo $cedula. ' '.$nombres. ' ' .$apellidos;
+echo $cedula. '  '.$nombres. '  ' .$apellidos;
 ?>
